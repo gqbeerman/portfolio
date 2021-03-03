c77a14253adacfd034ac905448a5746808ad5e38
gqbeerman <gqbeerman@yahoo.com>
2021-03-02 19:39:10 -0800

    messing with tilt

--- src/js/tilt.js
+++ src/js/tilt.js
@@ -3,7 +3,15 @@ import tilt from 'tilt.js';
 const initTilt = () => {
   // Projects images
   $('.project-wrapper__image a div').tilt({
-    maxTilt: 3,
+    maxTilt: 10,
+    perspective: 1000,
+    easing: "cubic-bezier(0.3, 0.98, 0.52, 0.99)",
+    scale: 1.1,
+    speed: 300,
+    transition: true,
+    reset: false,
+    glare: true,
+    maxGlare: .5,
   });
 };
 
